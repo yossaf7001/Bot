507a8957dc05572eb590f76c5a3e458b2bea1309
yossaf7001 <42251594+yossaf7001@users.noreply.github.com>
2018-08-09 22:44:10 +0200

    Create at.js

---
+++ cmd/at.js
@@ -0,0 +1,48 @@
+var Discord = require("discord.js");
+  exports.run = async (bot, message, args = []) => {
+
+      
+ var ids = [
+            "20",
+            "1",
+            "13",
+            "18",
+            "17",
+            "9",
+            "31",
+            "22",
+            "23",
+            "2",
+            "11",
+            "19",
+            "24",
+            "25",
+            "12",
+            "33"
+            ]
+            const randomizer = Math.floor(Math.random()*ids.length);
+            const args = message.content.split(" ").slice(1).join(" ")
+    if (!args) return message.channel.send("**اكتب محتوي الانجاز**");
+    const image = new Discord.Attachment(`https://www.minecraftskinstealer.com/achievement/a.php?i=${ids[randomizer]}&h=Achievement Get!&t=${args}`, "achievement.png");
+message.channel.send(image)
+
+
+
+
+
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [""],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "at",
+  description: "give u minecraft advancement"
+    category: "info",
+  usage: " at <input>"
+};
