cc14b8c2baf84a4be35e2f563ca791115412c36f
Topeyz <42250154+Topeyz@users.noreply.github.com>
2018-08-09 22:01:37 +0200

    Update invite.js

--- cmd/invite.js
+++ cmd/invite.js
@@ -1,1 +1,26 @@
+var Discord = require("discord.js");
+  exports.run = async (bot, message, args = []) => {
 
+      
+
+
+
+
+
+
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [""],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "invite",
+  description: "description",
+    category: "music",
+  usage: "usage"
+};
